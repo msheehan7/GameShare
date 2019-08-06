dbf5de1c1ba05346c77fb55b3e052bc1fa896a27
msheehan7 <52415615+msheehan7@users.noreply.github.com>
2019-08-06 14:55:50 -0400

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,24 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCzouSoNoVCIFO2QdzXyvpUlrAxP7ABJZk",
+    authDomain: "gameshare-1b851.firebaseapp.com",
+    databaseURL: "https://gameshare-1b851.firebaseio.com",
+    projectId: "gameshare-1b851",
+    storageBucket: "gameshare-1b851.appspot.com",
+    messagingSenderId: "804343057431",
+    appId: "1:804343057431:web:10ba27aee0a934dc"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  var db = firebase.firestore();
+
+  const auth = firebase.auth();
+
+
+
+  var user = firebase.auth().currentUser
+  if (user!=null) {
+    console.log('signed in')
+      
+  }else{
+      console.log('signed out')
+  }

60a82a4fb19e30279f63d8055618b0da97cc5461
msheehan7 <52415615+msheehan7@users.noreply.github.com>
2019-08-09 07:51:23 -0400

    Update supersmashbros.js

--- tradePages/supersmashbros.js
+++ tradePages/supersmashbros.js
@@ -125,7 +125,7 @@ function addGame(superSmash) {
                     //Use a sleep method to call after a certain amount of time
                     db.collection('superSmash').doc(superSmash.id).delete();
                     
-                      window.location.href = "tradeConfirmationPage.html"
+                      window.location.href = "../tradeConfirmationPage/tradeConfirmationPage.html"
                     
                                 }//else{
                                 // alert('An error has occured. Please re-save your game to inventory')

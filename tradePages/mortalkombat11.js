7a2166a749a3b0650eb0fb2478f14b63c252aa9e
msheehan7 <52415615+msheehan7@users.noreply.github.com>
2019-08-09 07:49:48 -0400

    Update mortalkombat11.js

--- tradePages/mortalkombat11.js
+++ tradePages/mortalkombat11.js
@@ -125,7 +125,7 @@ function addGame(mortalK) {
                     //Use a sleep method to call after a certain amount of time
                     db.collection('mortalK').doc(mortalK.id).delete();
                     
-                      window.location.href = "tradeConfirmationPage.html"
+                      window.location.href = "../tradeConfirmationPage/tradeConfirmationPage.html"
                     
                                 }//else{
                                 // alert('An error has occured. Please re-save your game to inventory')

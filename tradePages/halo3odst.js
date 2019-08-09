3321966bd367027bbf26175f1fd0045c2fb74f23
msheehan7 <52415615+msheehan7@users.noreply.github.com>
2019-08-09 07:46:22 -0400

    Update halo3odst.js

--- tradePages/halo3odst.js
+++ tradePages/halo3odst.js
@@ -125,7 +125,7 @@ function addGame(halo3) {
                     //Use a sleep method to call after a certain amount of time
                     db.collection('halo3').doc(halo3.id).delete();
                     
-                      window.location.href = "tradeConfirmationPage.html"
+                      window.location.href = "../tradeConfirmationPage/tradeConfirmationPage.html"
                     
                                 }//else{
                                 // alert('An error has occured. Please re-save your game to inventory')

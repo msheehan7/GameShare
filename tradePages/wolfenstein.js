6673a782336fbe88000c6634199a7de1f7897134
msheehan7 <52415615+msheehan7@users.noreply.github.com>
2019-08-09 07:52:09 -0400

    Update wolfenstein.js

--- tradePages/wolfenstein.js
+++ tradePages/wolfenstein.js
@@ -125,7 +125,7 @@ function addGame(wolfenstein) {
                     //Use a sleep method to call after a certain amount of time
                     db.collection('wolfenstein').doc(wolfenstein.id).delete();
                     
-                      window.location.href = "tradeConfirmationPage.html"
+                      window.location.href = "../tradeConfirmationPage/tradeConfirmationPage.html"
                     
                                 }//else{
                                 // alert('An error has occured. Please re-save your game to inventory')

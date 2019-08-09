b0d3b43d9b1b6fe9dc5189138b68bedc71dc9998
msheehan7 <52415615+msheehan7@users.noreply.github.com>
2019-08-09 07:46:51 -0400

    Update callofdutymodern.js

--- tradePages/callofdutymodern.js
+++ tradePages/callofdutymodern.js
@@ -124,7 +124,7 @@ function addGame(codModern) {
                     //Use a sleep method to call after a certain amount of time
                     db.collection('codModern').doc(codModern.id).delete();
                     
-                      window.location.href = "tradeConfirmationPage.html"
+                      window.location.href = "../tradeConfirmationPage/tradeConfirmationPage.html"
                     
                                 }//else{
                                 // alert('An error has occured. Please re-save your game to inventory')
